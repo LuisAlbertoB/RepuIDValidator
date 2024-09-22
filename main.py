feac8aa923ee56706df0a2d9c26e58e29709269d
Luis Alberto Batalla Gonzalez <luisalbagoz@localhost.localdomain>
2024-09-21 23:22:47 -0600

    Ahora el automata lee y genera archivos de texto

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
 from tabla_transiciones import tabla
 
-estado = 0
+def evaluar_cadena(cadena):
+    estado = 0
+    for character in cadena:
+        if (estado, character) in tabla:
+            estado = tabla[(estado, character)]
+        else:
+            return False 
+    return estado == 17
 
-print("Ingrese una cadena a evaluar:")
-cadena = input()
+with open('test.txt', 'r') as archivo:
+    cadenas = archivo.read().splitlines()
 
-# Ciclo para recorrer la cadena
-for character in cadena:
-    if (estado, character) in tabla:
-        estado = tabla[(estado, character)]
-    else:
-        estado = 'E'  # Estado de error
-        break
-
-# Verificación del estado final
-if estado == 17:
-    print("Cadena Válida")
-else:
-    print("Cadena No Válida")
+with open('report.txt', 'w') as report_file:
+    for cadena in cadenas:
+        if evaluar_cadena(cadena):
+            report_file.write(f"{cadena}\n")
